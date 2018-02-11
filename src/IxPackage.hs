281748fba954e7713d2b2c1fa2cae60902bde8ac
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2018-02-11 23:14:40 +0100

    minor: changes (IxPackage.hs, IxWriter.hs)

--- src/IxPackage.hs
+++ src/IxPackage.hs
@@ -1,32 +1,28 @@
-{-# LANGUAGE ConstraintKinds      #-}
-{-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE GADTs                #-}
-{-# LANGUAGE KindSignatures       #-}
-{-# LANGUAGE PolyKinds            #-}
-{-# LANGUAGE Rank2Types           #-}
-{-# LANGUAGE RebindableSyntax     #-}
-{-# LANGUAGE TypeFamilies         #-}
-{-# LANGUAGE TypeInType           #-}
-{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module IxPackage where
 
-import           Control.Monad.Writer.Lazy (Monad, Monoid, tell)
-import           Data.Proxy
-import           Data.String
-import           GHC.TypeLits              ()
-import           IxWriter
+import Control.Monad.Writer.Lazy (Monad, Monoid, return, tell)
+import Data.Proxy
+import Data.String
+import GHC.Exts
+import GHC.TypeLits ()
+import IxWriter
 
-return
-    :: (Monoid o, IxMonad m)
-    => rs -> m o pr po rs
-return = ireturn
-
-(>>=)
-    :: (Monoid o, IxMonad m)
-    => m o pr1 po1 rs1 -> (rs1 -> m o po1 pr2 rs2) -> m o pr1 po2 rs2
+-- return = ireturn
 (>>=) = ibind
 
 v >> w =
@@ -38,17 +34,10 @@ v >> w =
 -- (<'Foo>, <'Bar>, <'Baz>). For each of them, we can construct an inhabitant value
 -- with <(Proxy :: Proxy 'Bar)>.
 data PackageState
-    = Loaded
+    = Needed
+    | Loaded
     | Initialized
     | Running
-
-data PackageType
-    = Major
-    | Minor
-
-data FileType
-    = AllOf [String]
-    | All
 
 type PackageNames = [PackageName]
 
@@ -58,37 +47,36 @@ data PackageName
     | Baz
 
 data Package (name :: pn) (state :: PackageState)
-     (t :: PackageType) where
-        Load ::
-          Proxy (p :: pn) -> Proxy (t :: PackageType) -> Package p 'Loaded t
-        Init ::
-          Package (p :: pn) 'Loaded (t :: PackageType) ->
-            Package p 'Initialized t
-        EnableMajorOn ::
-          Package (p :: pn) 'Initialized 'Major ->
-            FileType -> Package p 'Running 'Major
+
+type PNeeded p = Package p 'Needed
+
+type PLoaded p = Package p 'Loaded
+
+type PInited p = Package p 'Initialized
 
 load
-    :: Proxy (p :: pnk)
-    -> Proxy (t :: PackageType)
-    -> SIxWriter () (Package p 'Loaded t) ()
-load p t = do
+    :: (PNeeded p :^: ctx0)
+    => Proxy (p :: pnk) -> SIxWriter ctx0 (PLoaded p ': ctx0) ()
+load p = do
     IXW (tell "Load xyz")
 
 init
-    :: Proxy (p :: pnk)
-    -> Proxy (t :: PackageType)
-    -> SIxWriter (Package p 'Loaded t) (Package p 'Initialized t) ()
-init p t = do
+    :: (PLoaded p :^: ctx0)
+    => Proxy (p :: pnk) -> SIxWriter ctx0 (PInited p ': ctx0) ()
+init p = do
     IXW (tell "Load xyz")
 
 p1 = Proxy :: Proxy 'Bar
 
 p2 = Proxy :: Proxy 'Foo
 
-major = Proxy :: Proxy 'Major
-
-myConfig :: SIxWriter () (Package 'Bar 'Initialized 'Major) ()
-myConfig = do
-    load p1 major
-    init p1 major
+-- This should typecheck, but cant prove ctx1 ~ ...
+-- myBarConfig
+--     :: (PNeeded 'Bar :^: ctx0, PInited 'Bar :^: ctx1)
+--     => IxWriter String ctx0 ctx1 ()
+myBarConfig
+    :: (PNeeded 'Bar :^: ctx0)
+    => IxWriter String ctx0 (PInited 'Bar : PLoaded 'Bar : ctx0) ()
+myBarConfig = do
+    load p1
+    init p1
--- src/IxWriter.hs
+++ src/IxWriter.hs
@@ -1,22 +1,38 @@
+{-# LANGUAGE AllowAmbiguousTypes       #-}
+{-# LANGUAGE ConstraintKinds           #-}
+{-# LANGUAGE DataKinds                 #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE GADTs                     #-}
+{-# LANGUAGE KindSignatures            #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE PolyKinds                 #-}
+{-# LANGUAGE Rank2Types                #-}
+{-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE TypeInType                #-}
+{-# LANGUAGE TypeOperators             #-}
+{-# LANGUAGE UndecidableInstances      #-}
+
 module IxWriter where
 
 import           Control.Monad.Writer.Lazy
+import           Data.Kind
+import           GHC.Exts                  ()
+
+type family (:^:) (e :: u) (es :: [u]) :: Constraint where
+        x :^: (x ': xs) = ()
+        y :^: (x ': xs) = y :^: xs
 
 class IxMonad m  where
-    ireturn
-        :: (Monoid o)
-        => rs -> m o pr po rs
     ibind
         :: (Monoid o)
-        => m o pr1 po1 rs1 -> (rs1 -> m o po1 pr2 rs2) -> m o pr1 po2 rs2
+        => m o ctx0 ctx1 rs1 -> (rs1 -> m o ctx1 ctx2 rs2) -> m o ctx0 ctx2 rs2
 
 -- Interpret like this:
-newtype IxWriter o pr po rs = IXW
+newtype IxWriter o ctx0 ctx1 rs = IXW
     { unIXW :: Writer o rs
     }
 
 instance IxMonad IxWriter where
-    ireturn = IXW . return
     ibind (IXW w) f = IXW (w >>= unIXW . f)
 
 type SIxWriter pr po rs = IxWriter String pr po rs
